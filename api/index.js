793b7175b2f98c3e923421822a73e8bc0fa3d7e1
Hari karan <harikaranabba@gmail.com>
2026-09-18 16:48:02 +0530

    diagnose Vercel environment

--- api/index.js
+++ api/index.js
@@ -1,1 +1,8 @@
-module.exports = require("../server");
+module.exports = (req, res) => {
+  res.json({
+    SUPABASE_URL: !!process.env.SUPABASE_URL,
+    SUPABASE_KEY: !!process.env.SUPABASE_KEY,
+    RAZORPAY_KEY_ID: !!process.env.RAZORPAY_KEY_ID,
+    RAZORPAY_KEY_SECRET: !!process.env.RAZORPAY_KEY_SECRET
+  });
+};
